cf3afe7f2c422e33ce7ec6117b8223daf4e6b515
AbhinavlKumar <25LBCS3095@culkomail.in>
2026-03-29 18:03:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        string st;
+        st.reserve(s.size());
         for(char c : s) {
-            if(c == '(' || c == '{' || c == '[') st.push(c);
+            if(c=='(' || c=='{' || c=='[') st.push_back(c);
             else {
                 if(st.empty()) return false;
-                char t = st.top(); st.pop();
-                if((c == ')' && t != '(') || 
-                   (c == '}' && t != '{') || 
-                   (c == ']' && t != '[')) return false;
+                char t = st.back();
+                if((c==')' && t!='(') ||
+                   (c=='}' && t!='{') ||
+                   (c==']' && t!='[')) return false;
+                st.pop_back();
             }
         }
         return st.empty();
