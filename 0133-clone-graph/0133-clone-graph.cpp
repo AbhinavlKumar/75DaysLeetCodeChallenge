50ce34c9e32781004732e424c797e2cd37d4b5a4
Abhinav Kumar <25LBCS3095@culkomail.in>
2026-05-29 11:56:49 +0530

    Time: 5 ms (24.8%), Space: 12.2 MB (26.28%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.cpp
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -1,32 +1,24 @@
 class Solution {
-private:
-    // Hash map to store the original node as the key and the cloned node as the value
-    unordered_map<Node*, Node*> visited;
-
 public:
+    unordered_map<Node*, Node*> mp;
+
     Node* cloneGraph(Node* node) {
-        // Base case: if the graph is empty
-        if (!node) {
+        if (node == nullptr)
             return nullptr;
-        }
 
-        // If the node has already been cloned, return the cloned reference from the map
-        if (visited.find(node) != visited.end()) {
-            return visited[node];
-        }
+        // If already cloned, return the clone
+        if (mp.find(node) != mp.end())
+            return mp[node];
 
-        // Create a clone for the current node
-        Node* cloneNode = new Node(node->val);
-        
-        // Add it to the map BEFORE iterating through its neighbors to avoid infinite recursion
-        visited[node] = cloneNode;
+        // Create clone of current node
+        Node* clone = new Node(node->val);
+        mp[node] = clone;
 
-        // Iterate through the neighbors of the original node
+        // Clone all neighbors
         for (Node* neighbor : node->neighbors) {
-            // Recursively clone each neighbor and add it to the cloned node's neighbor list
-            cloneNode->neighbors.push_back(cloneGraph(neighbor));
+            clone->neighbors.push_back(cloneGraph(neighbor));
         }
 
-        return cloneNode;
+        return clone;
     }
 };
