3040fa31dbd776958fe5baebf91de8a677bd4504
Abhinav Kumar <25LBCS3095@culkomail.in>
2026-08-03 22:27:52 +0530

    Time: 48 ms (83.09%), Space: 151.4 MB (26.47%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -3,12 +3,23 @@ class MinStack {
 public:
     void push(int val) {
         st.push(val);
-        if (mn.empty() || val <= mn.top()) mn.push(val);
+        if (mn.empty() || val <= mn.top()) {
+            mn.push(val);
+        }
     }
+    
     void pop() {
-        if (st.top() == mn.top()) mn.pop();
+        if (st.top() == mn.top()) {
+            mn.pop();
+        }
         st.pop();
     }
-    int top() const { return st.top(); }
-    int getMin() const { return mn.top(); }
+    
+    int top() const {
+        return st.top();
+    }
+    
+    int getMin() const {
+        return mn.top();
+    }
 };
