49544c07fd4947e24ab5da3e0ebc1a286cd50b4a
Abhinav Kumar <25LBCS3095@culkomail.in>
2026-08-13 21:35:53 +0530

    Time: 318 ms (39.6%), Space: 113 MB (31.55%) - LeetHub

---
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.cpp
@@ -0,0 +1,91 @@
+class Solution {
+public:
+    struct Node {
+        int len;
+        int pref;
+        int suff;
+        int best;
+        char leftChar;
+        char rightChar;
+    };
+
+    vector<Node> tree;
+
+    Node merge(Node a, Node b) {
+        if (a.len == 0) return b;
+        if (b.len == 0) return a;
+
+        Node res;
+        res.len = a.len + b.len;
+        res.leftChar = a.leftChar;
+        res.rightChar = b.rightChar;
+
+        res.pref = a.pref;
+        res.suff = b.suff;
+        res.best = max(a.best, b.best);
+
+        if (a.rightChar == b.leftChar) {
+            res.best = max(res.best, a.suff + b.pref);
+
+            if (a.pref == a.len)
+                res.pref = a.len + b.pref;
+
+            if (b.suff == b.len)
+                res.suff = b.len + a.suff;
+        }
+
+        return res;
+    }
+
+    void build(int node, int l, int r, string &s) {
+        if (l == r) {
+            tree[node] = {1, 1, 1, 1, s[l], s[l]};
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid, s);
+        build(node * 2 + 1, mid + 1, r, s);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r, int idx, char c) {
+        if (l == r) {
+            tree[node] = {1, 1, 1, 1, c, c};
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (idx <= mid)
+            update(node * 2, l, mid, idx, c);
+        else
+            update(node * 2 + 1, mid + 1, r, idx, c);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    vector<int> longestRepeating(string s, string queryCharacters,
+                                 vector<int>& queryIndices) {
+
+        int n = s.size();
+
+        tree.resize(4 * n);
+
+        build(1, 0, n - 1, s);
+
+        vector<int> ans;
+
+        for (int i = 0; i < queryIndices.size(); i++) {
+            update(1, 0, n - 1,
+                   queryIndices[i],
+                   queryCharacters[i]);
+
+            ans.push_back(tree[1].best);
+        }
+
+        return ans;
+    }
+};
