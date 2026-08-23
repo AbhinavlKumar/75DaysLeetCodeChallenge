38ed3276494bce66b95603fc422979a151bc152d
Abhinav Kumar <25LBCS3095@culkomail.in>
2026-08-23 18:54:33 +0530

    Time: 1 ms (77.62%), Space: 14 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int sumL = 0, sumR = 0;
+        int qL = 0, qR = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (i < n / 2) {
+                if (num[i] == '?') qL++;
+                else sumL += num[i] - '0';
+            } else {
+                if (num[i] == '?') qR++;
+                else sumR += num[i] - '0';
+            }
+        }
+        return 2 * (sumL - sumR) != 9 * (qR - qL);
+    }
+};
